7e5ba3ecfccb7fb7d8307e18cc8cb9fa952b5d64
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-08-31 07:29:30 +0300

    Enable block layout customization in partner portal

--- partner/config.js
+++ partner/config.js
@@ -1,1 +1,9 @@
 window.A4PRINT_PARTNER_CONFIG={supabaseUrl:'https://qgakliolffnwkymoqvzn.supabase.co',supabasePublishableKey:'sb_publishable_WbZxATu_lxqWF21jR_qFag_fcEeVIMu'};
+
+(function loadPartnerLayout(){
+  if (/\/partner\/login\.html$/.test(location.pathname)) return;
+  const s=document.createElement('script');
+  s.src='../admin/layout.js?v=20260831-3';
+  s.defer=true;
+  document.head.appendChild(s);
+})();
