ae6d9d453d5dfd0ed0e20e5bd17574cf04299f69
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-08-31 19:36:08 +0300

    Load partner service quick search

--- admin/config.js
+++ admin/config.js
@@ -9,7 +9,7 @@ window.A4PRINT_CONFIG = {
 
 (function loadHubUi(){
   const base = new URL('./', document.currentScript?.src || location.href);
-  const load = (file, version='20260831-16') => {
+  const load = (file, version='20260831-17') => {
     const s = document.createElement('script');
     s.src = new URL(file, base).href + '?v=' + version;
     s.async = false;
@@ -25,4 +25,5 @@ window.A4PRINT_CONFIG = {
   load('navigation.js');
   load('layout.js');
   load('help-context.js');
+  if (/\/admin\/partners\.html$/.test(location.pathname)) load('partners-search.js');
 })();
