7059bdd2db78682c2a0ab6bfdf8de6ed8c9f9388
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-08-31 11:19:04 +0300

    Apply global UI fixes to Partner portal

--- partner/config.js
+++ partner/config.js
@@ -1,8 +1,8 @@
 window.A4PRINT_PARTNER_CONFIG={supabaseUrl:'https://qgakliolffnwkymoqvzn.supabase.co',supabasePublishableKey:'sb_publishable_WbZxATu_lxqWF21jR_qFag_fcEeVIMu'};
 
 (function loadPartnerUi(){
-  const scripts=['../admin/theme.js?v=20260831-7'];
-  if (!/\/partner\/login\.html$/.test(location.pathname)) scripts.push('../admin/layout.js?v=20260831-5','./help.js?v=20260831-1','./crm-entry.js?v=20260831-1','./marketplace-nav.js?v=20260831-1');
+  const scripts=['../admin/theme.js?v=20260831-15','../admin/ui-fixes.js?v=20260831-15'];
+  if (!/\/partner\/login\.html$/.test(location.pathname)) scripts.push('../admin/layout.js?v=20260831-15','./help.js?v=20260831-2','./crm-entry.js?v=20260831-2','./marketplace-nav.js?v=20260831-2');
   scripts.forEach(src=>{
     const s=document.createElement('script');
     s.src=src;
