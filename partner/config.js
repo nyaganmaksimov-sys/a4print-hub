e8a53fa33b0959edd8f13e5fa82ae9eb918eb63c
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-08-31 10:58:08 +0300

    Load marketplace navigation in Partner CRM

--- partner/config.js
+++ partner/config.js
@@ -2,7 +2,7 @@ window.A4PRINT_PARTNER_CONFIG={supabaseUrl:'https://qgakliolffnwkymoqvzn.supabas
 
 (function loadPartnerUi(){
   const scripts=['../admin/theme.js?v=20260831-7'];
-  if (!/\/partner\/login\.html$/.test(location.pathname)) scripts.push('../admin/layout.js?v=20260831-5','./help.js?v=20260831-1','./crm-entry.js?v=20260831-1');
+  if (!/\/partner\/login\.html$/.test(location.pathname)) scripts.push('../admin/layout.js?v=20260831-5','./help.js?v=20260831-1','./crm-entry.js?v=20260831-1','./marketplace-nav.js?v=20260831-1');
   scripts.forEach(src=>{
     const s=document.createElement('script');
     s.src=src;
