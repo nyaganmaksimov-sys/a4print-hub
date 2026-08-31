02b409c3a0dd5f8d49e6d3fc14e8198ce026e26e
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-08-31 07:55:27 +0300

    Enable help in partner portal

--- partner/config.js
+++ partner/config.js
@@ -1,9 +1,11 @@
 window.A4PRINT_PARTNER_CONFIG={supabaseUrl:'https://qgakliolffnwkymoqvzn.supabase.co',supabasePublishableKey:'sb_publishable_WbZxATu_lxqWF21jR_qFag_fcEeVIMu'};
 
-(function loadPartnerLayout(){
+(function loadPartnerUi(){
   if (/\/partner\/login\.html$/.test(location.pathname)) return;
-  const s=document.createElement('script');
-  s.src='../admin/layout.js?v=20260831-3';
-  s.defer=true;
-  document.head.appendChild(s);
+  ['../admin/layout.js?v=20260831-5','./help.js?v=20260831-1'].forEach(src=>{
+    const s=document.createElement('script');
+    s.src=src;
+    s.defer=true;
+    document.head.appendChild(s);
+  });
 })();
