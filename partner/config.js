e9132e29b5d82ab7a3e295b3941f76c656c9c050
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-08-31 09:42:14 +0300

    Load shared theme in partner portal

--- partner/config.js
+++ partner/config.js
@@ -1,8 +1,9 @@
 window.A4PRINT_PARTNER_CONFIG={supabaseUrl:'https://qgakliolffnwkymoqvzn.supabase.co',supabasePublishableKey:'sb_publishable_WbZxATu_lxqWF21jR_qFag_fcEeVIMu'};
 
 (function loadPartnerUi(){
-  if (/\/partner\/login\.html$/.test(location.pathname)) return;
-  ['../admin/layout.js?v=20260831-5','./help.js?v=20260831-1'].forEach(src=>{
+  const scripts=['../admin/theme.js?v=20260831-7'];
+  if (!/\/partner\/login\.html$/.test(location.pathname)) scripts.push('../admin/layout.js?v=20260831-5','./help.js?v=20260831-1');
+  scripts.forEach(src=>{
     const s=document.createElement('script');
     s.src=src;
     s.defer=true;
