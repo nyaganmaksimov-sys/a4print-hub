642adebd1c4e3ced7ea7ecd336bdedd715a5d214
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-01 10:22:06 +0300

    Load courier delivery handoff in Partner CRM

--- partner/config.js
+++ partner/config.js
@@ -2,7 +2,7 @@ window.A4PRINT_PARTNER_CONFIG={supabaseUrl:'https://qgakliolffnwkymoqvzn.supabas
 
 (function loadPartnerUi(){
   const scripts=['./pwa.js?v=20260901-2','../admin/theme.js?v=20260831-15','../admin/ui-fixes.js?v=20260831-15'];
-  if (!/\/partner\/login\.html$/.test(location.pathname)) scripts.push('../admin/layout.js?v=20260831-15','./help.js?v=20260831-2','./crm-entry.js?v=20260831-2','./marketplace-nav.js?v=20260831-2');
+  if (!/\/partner\/login\.html$/.test(location.pathname)) scripts.push('../admin/layout.js?v=20260831-15','./help.js?v=20260831-2','./crm-entry.js?v=20260831-2','./marketplace-nav.js?v=20260831-2','./courier-orders.js?v=20260901-1');
   scripts.forEach(src=>{
     const s=document.createElement('script');
     s.src=src;
