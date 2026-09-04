19f5237509beea8db3f67b7c28ff9e11df14e1e4
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-04 11:39:33 +0300

    Load staff structure enhancements

--- admin/config.js
+++ admin/config.js
@@ -9,7 +9,7 @@ window.A4PRINT_CONFIG = {
 
 (function loadHubUi(){
   const base = new URL('./', document.currentScript?.src || location.href);
-  const load = (file, version='20260831-17') => {
+  const load = (file, version='20260904-1') => {
     const s = document.createElement('script');
     s.src = new URL(file, base).href + '?v=' + version;
     s.async = false;
@@ -26,4 +26,5 @@ window.A4PRINT_CONFIG = {
   load('layout.js');
   load('help-context.js');
   if (/\/admin\/partners\.html$/.test(location.pathname)) load('partners-search.js');
+  if (/\/admin\/employees\.html$/.test(location.pathname)) load('employees-structure.js');
 })();
