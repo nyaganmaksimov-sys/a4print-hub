5a4870275dfb016a82b23c88713959e8d0b944ad
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-04 14:02:27 +0300

    Load updated chat notifications across HUB

--- admin/config.js
+++ admin/config.js
@@ -9,7 +9,7 @@ window.A4PRINT_CONFIG = {
 
 (function loadHubUi(){
   const base = new URL('./', document.currentScript?.src || location.href);
-  const load = (file, version='20260904-7') => {
+  const load = (file, version='20260904-8') => {
     const s = document.createElement('script');
     s.src = new URL(file, base).href + '?v=' + version;
     s.async = false;
@@ -25,6 +25,6 @@ window.A4PRINT_CONFIG = {
   load('navigation.js');
   load('layout.js');
   load('help-context.js');
-  if (!/\/admin\/(login|register|pending)\.html$/.test(location.pathname)) load('chat-notifications.js','20260904-1');
+  if (!/\/admin\/(login|register|pending)\.html$/.test(location.pathname)) load('chat-notifications.js','20260904-2');
   if (/\/admin\/partners\.html$/.test(location.pathname)) load('partners-search.js');
 })();
