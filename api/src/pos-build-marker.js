191fbb6744c54ae1768a1b2d16ab9bbccdbecc39
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-05 19:21:37 +0300

    Bump POS build for shift sanity fix

--- api/src/pos-build-marker.js
+++ api/src/pos-build-marker.js
@@ -1,13 +1,13 @@
 import express from 'express';
 
-const BUILD='20260905-mslive5';
+const BUILD='20260905-mslive6';
 const originalGet=express.application.get;
 let added=false;
 
 express.application.get=function patchedGet(path,...handlers){
   if(!added){
     added=true;
-    originalGet.call(this,'/api/v1/pos/build',(_req,res)=>res.json({success:true,posBuild:BUILD,shiftSource:'moysklad-direct'}));
+    originalGet.call(this,'/api/v1/pos/build',(_req,res)=>res.json({success:true,posBuild:BUILD,shiftSource:'moysklad-direct',shiftSanity:true}));
   }
   return originalGet.call(this,path,...handlers);
 };
