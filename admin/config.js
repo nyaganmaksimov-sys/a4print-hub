0b5d3037e019cfeca306cc40af8f5e079a1ef858
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-05 10:05:36 +0300

    Bump navigation cache after logo fix

--- admin/config.js
+++ admin/config.js
@@ -78,7 +78,7 @@ window.A4PRINT_CONFIG = {
 
   if (isManager) load('manager-runtime.js','20260905-4');
   if (isSettings) load('auth-settings.js','20260905-2');
-  load('navigation.js','20260905-5');
+  load('navigation.js','20260905-6');
   load('workspace-clean.js','20260904-2');
   load('nav-accordion.js','20260904-3');
   load('modern-ui.js','20260904-1');
