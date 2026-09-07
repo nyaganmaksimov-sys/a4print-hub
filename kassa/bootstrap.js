6303a0d4c0af671e2d6fa3059e6e5fae11074974
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-07 10:15:05 +0300

    Enable receipt and customer sale flow

--- kassa/bootstrap.js
+++ kassa/bootstrap.js
@@ -1,6 +1,6 @@
 (()=>{
   'use strict';
-  const VERSION='20260907-saleguard1';
+  const VERSION='20260907-salefinish1';
   const $=id=>document.getElementById(id);
 
   function loadScript(src){
@@ -51,9 +51,11 @@
     await loadScript('./modules.js');
     await loadScript('./ui.js');
     await loadStyle('./shift-layout-fix.css');
+    await loadStyle('./sale-finish.css');
     await loadScript('./shift-operator.js');
     await loadScript('./shift-state-sync.js');
     await loadScript('./sale-submit-guard.js');
+    await loadScript('./sale-finish.js');
     await loadScript('./sale-view-fix.js');
     await loadScript('./startup-shift.js');
     window.__A4_KASSA_BOOT_OK__=true;
