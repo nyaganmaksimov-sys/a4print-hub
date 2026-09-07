959789e5525b6982a9641124058fae6a89f043eb
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-07 15:19:05 +0300

    Expose cash operations capability

--- api/src/pos-build-marker.js
+++ api/src/pos-build-marker.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const BUILD='20260907-shiftfresh1';
+const BUILD='20260907-cashops1';
 const originalGet=express.application.get;
 let added=false;
 
@@ -15,7 +15,8 @@ express.application.get=function patchedGet(path,...handlers){
       freshManualShift:true,
       shiftIdSummary:true,
       paymentBreakdown:true,
-      saleIdempotency:true
+      saleIdempotency:true,
+      cashOperations:true
     }));
   }
   return originalGet.call(this,path,...handlers);
