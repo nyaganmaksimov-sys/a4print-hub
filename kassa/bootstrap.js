b74c8ff113e2bfc7e8f4f21f5d98a3e369df4c32
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-07 10:10:10 +0300

    Load duplicate sale submission guard

--- kassa/bootstrap.js
+++ kassa/bootstrap.js
@@ -1,6 +1,6 @@
 (()=>{
   'use strict';
-  const VERSION='20260907-shiftpersist1';
+  const VERSION='20260907-saleguard1';
   const $=id=>document.getElementById(id);
 
   function loadScript(src){
@@ -53,6 +53,7 @@
     await loadStyle('./shift-layout-fix.css');
     await loadScript('./shift-operator.js');
     await loadScript('./shift-state-sync.js');
+    await loadScript('./sale-submit-guard.js');
     await loadScript('./sale-view-fix.js');
     await loadScript('./startup-shift.js');
     window.__A4_KASSA_BOOT_OK__=true;
