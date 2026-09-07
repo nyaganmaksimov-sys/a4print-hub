d536f8c0464f9f51a2b680a42d0ab2f421f5c603
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-07 09:25:18 +0300

    Start Kassa on shift screen

--- kassa/bootstrap.js
+++ kassa/bootstrap.js
@@ -1,6 +1,6 @@
 (()=>{
   'use strict';
-  const VERSION='20260907-shiftfresh1';
+  const VERSION='20260907-shiftstart1';
   const $=id=>document.getElementById(id);
 
   function loadScript(src){
@@ -53,6 +53,7 @@
     await loadStyle('./shift-layout-fix.css');
     await loadScript('./shift-operator.js');
     await loadScript('./shift-state-sync.js');
+    await loadScript('./startup-shift.js');
     window.__A4_KASSA_BOOT_OK__=true;
   }
 
