4b9a6197bb4aadd872dccd54d653b438924375a3
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-07 12:54:08 +0300

    Enable HUB receipt history

--- kassa/bootstrap.js
+++ kassa/bootstrap.js
@@ -1,6 +1,6 @@
 (()=>{
   'use strict';
-  const VERSION='20260907-salefinish1';
+  const VERSION='20260907-historyhub1';
   const $=id=>document.getElementById(id);
 
   function loadScript(src){
@@ -56,6 +56,7 @@
     await loadScript('./shift-state-sync.js');
     await loadScript('./sale-submit-guard.js');
     await loadScript('./sale-finish.js');
+    await loadScript('./history-hub.js');
     await loadScript('./sale-view-fix.js');
     await loadScript('./startup-shift.js');
     window.__A4_KASSA_BOOT_OK__=true;
