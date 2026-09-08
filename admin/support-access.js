4e1fc2b407114cc58479b894706a3c8c92921efd
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-08 21:08:53 +0300

    Connect equipment maintenance analytics to reports

--- admin/support-access.js
+++ admin/support-access.js
@@ -53,8 +53,9 @@
       if(document.getElementById(id))return;
       const s=document.createElement('script');s.id=id;s.type='module';s.src=src;document.head.appendChild(s);
     };
-    if(/\/admin\/(?:index\.html)?$/.test(path))add('./dashboard-equipment-widget.js?v=20260908-1','a4-dashboard-equipment-module');
+    if(/\/admin\/(?:index\.html)?$/.test(path))add('./dashboard-equipment-widget.js?v=20260908-2','a4-dashboard-equipment-module');
     if(/\/admin\/equipment\.html$/.test(path))add('./equipment-service-kpis.js?v=20260908-1','a4-equipment-service-kpis-module');
+    if(/\/admin\/reports\.html$/.test(path))add('./reports-equipment-widget.js?v=20260908-1','a4-reports-equipment-module');
   }
 
   function isolateSupportNav(){
