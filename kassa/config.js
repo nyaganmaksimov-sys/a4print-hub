b292545e95b6d9dd9c60a740db71434afab92f71
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-10 09:14:07 +0300

    Route KASSA API through Cloudflare custom domain

--- kassa/config.js
+++ kassa/config.js
@@ -1,7 +1,7 @@
 window.A4PRINT_CONFIG={
   supabaseUrl:'https://qgakliolffnwkymoqvzn.supabase.co',
   supabasePublishableKey:'sb_publishable_WbZxATu_lxqWF21jR_qFag_fcEeVIMu',
-  apiBaseUrl:'https://a4print-hub-api.onrender.com'
+  apiBaseUrl:'https://api.a4print-hub.ru'
 };
 
 // KASSA: proxy Supabase through HUB first, but never let a blocked API domain
