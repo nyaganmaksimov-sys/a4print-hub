0e39e599fe12a578bccabd75c0dedea104482d3f
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-11 09:35:00 +0300

    feat: preload Jarvis runtime health endpoint

--- api/src/boot.js
+++ api/src/boot.js
@@ -1,5 +1,6 @@
 const preloads = [
   '--import=./src/jarvis-network-resilience.js',
+  '--import=./src/jarvis-runtime-health.js',
   '--import=./src/pos-operator-patch.js',
   '--import=./src/pos-sale-idempotency.js',
   '--import=./src/pos-build-marker.js',
