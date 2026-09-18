79b5c6748532a165f41152db7929b87bcabca838
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-18 20:07:10 +0300

    Expose selected printer options safely

--- desktop-kassa/preload.js
+++ desktop-kassa/preload.js
@@ -4,5 +4,5 @@ contextBridge.exposeInMainWorld('A4KassaDesktop',Object.freeze({
   platform:process.platform,
   version:'2.1.0',
   printers:()=>ipcRenderer.invoke('a4-kassa:print'),
-  print:()=>ipcRenderer.invoke('a4-kassa:print-page')
+  print:(options={})=>ipcRenderer.invoke('a4-kassa:print-page',options)
 }));
