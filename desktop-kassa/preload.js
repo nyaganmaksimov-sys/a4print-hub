68109e1bc41e665878772e62b2b4eb4cc2327aed
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-18 20:04:02 +0300

    Expose safe printer API to KASSA

--- desktop-kassa/preload.js
+++ desktop-kassa/preload.js
@@ -1,6 +1,8 @@
-const {contextBridge}=require('electron');
+const {contextBridge,ipcRenderer}=require('electron');
 contextBridge.exposeInMainWorld('A4KassaDesktop',Object.freeze({
   desktop:true,
   platform:process.platform,
-  version:'2.1.0'
+  version:'2.1.0',
+  printers:()=>ipcRenderer.invoke('a4-kassa:print'),
+  print:()=>ipcRenderer.invoke('a4-kassa:print-page')
 }));
