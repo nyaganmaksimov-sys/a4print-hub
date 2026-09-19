1083256a877a7c6b2fe928307c6ef08bb4c9c136
nyaganmaksimov-sys <nyaganmaksimov@gmail.com>
2026-09-19 08:02:43 +0300

    Bypass crashing legacy Kassa login redirect in desktop (#195)
    * Keep desktop on native Kassa auth instead of crashing legacy login redirect
    
    * Fix desktop navigation guard syntax
    
    * Remove escaped newline literals from desktop source
    
    * Rewrite login navigation guard with valid JavaScript

--- desktop-kassa/main.js
+++ desktop-kassa/main.js
@@ -23,6 +23,13 @@ async function createWindow(){
     webPreferences:{partition,contextIsolation:true,nodeIntegration:false,sandbox:false,backgroundThrottling:false}
   });
   const reveal=()=>{if(!splash.isDestroyed())splash.destroy();if(mainWindow&&!mainWindow.isDestroyed()){mainWindow.show();mainWindow.focus();}};
+  mainWindow.webContents.on('will-navigate',(event,url)=>{
+    if(/^https:\/\/a4print-hub\.ru\/kassa\/login\.html(?:[?#]|$)/i.test(url)){
+      log('BYPASS LEGACY LOGIN REDIRECT',url);
+      event.preventDefault();
+      return;
+    }
+  });
   mainWindow.webContents.on('did-start-navigation',(_e,url,isInPlace,isMain)=>{if(isMain)log('NAV START',url)});
   mainWindow.webContents.on('did-redirect-navigation',(_e,url,isInPlace,isMain)=>{if(isMain)log('NAV REDIRECT',url)});
   mainWindow.webContents.on('did-navigate',(_e,url)=>log('NAV DONE',url));
